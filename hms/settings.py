d8ac4373a5f4fdfb4d58df4e6a18f1c749324853
Dennis Kamunya <kinc.developer@gmail.com>
2021-02-01 17:23:38 +0300

    Eddit settings'

--- hms/settings.py
+++ hms/settings.py
@@ -163,9 +163,9 @@ AUTH_USER_MODEL = 'users.CustomUser'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
+        'NAME': config('DB_NAME',''),
+        'USER': config('DB_USER',''),
+        'PASSWORD': config('DB_PASSWORD',''),
     }
 }
 
