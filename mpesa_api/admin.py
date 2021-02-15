f4bf43682552fd08be90c730750128708546a47e
Dennis Kamunya <kinc.developer@gmail.com>
2021-02-15 13:12:02 +0300

    Registrr mpesa calls and callbacks models in admin

--- mpesa_api/admin.py
+++ mpesa_api/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import MpesaPayment
+from .models import MpesaPayment,MpesaCalls,MpesaCallBacks
 
 
 admin.site.register(MpesaPayment)
+admin.site.register(MpesaCalls)
+admin.site.register(MpesaCallBacks)
